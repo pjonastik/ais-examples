7114554ba5db2c9ae09f5693ce41d10ba849dc0d
pjonastik <peto.jonastik@gmail.com>
2023-09-01 10:33:14 +0200

    B2L1 - move test class from parent to child module

--- b2lekce1/childmodule/src/main/java/org/example/App.java
+++ b2lekce1/childmodule/src/main/java/org/example/App.java
@@ -1,11 +1,6 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
