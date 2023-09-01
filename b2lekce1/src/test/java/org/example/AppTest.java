a88e501b027cfd53a10fb831956372ca174cfea4
pjonastik <peto.jonastik@gmail.com>
2023-09-01 08:46:18 +0200

    B2L1 - add JUnit and Hamcrest dependnecies

--- b2lekce1/src/test/java/org/example/AppTest.java
+++ b2lekce1/src/test/java/org/example/AppTest.java
@@ -1,14 +1,14 @@
 package org.example;
 
-//import org.junit.jupiter.api.Test;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class AppTest {
 
-//    @Test
-//    public void happyTest() {
-//        assertThat(true, equalTo(true));
-//    }
+    @Test
+    public void happyTest() {
+        assertThat(true, equalTo(true));
+    }
 }
